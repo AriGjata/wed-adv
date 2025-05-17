63c8fabad38253379579174e07951fdf6b0b8a26
AriGjata <arigjata112@gmail.com>
2025-05-17 04:43:15 -0700

    day 24

---
+++ day 24/main.js
@@ -0,0 +1,21 @@
+// var button1 = document.getElementById("btn1");
+
+var button1 = document.querySelector('button');
+var button2 = document.getElementById('btn2');
+var button3 = document.getElementById('btn3');
+
+button1.addEventListener('click', function(){
+    alert("hello from addeventlistener");
+})
+
+// button1.onclick = function(){
+//     alert("hello");
+// }
+
+button2.onmouseover = function(){
+    alert("mous is over the button!");
+}
+
+button3.onmouseleave = function(){
+    alert("mous has left the button!");
+}
